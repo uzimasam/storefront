6f71cd33214418ba2c881b0032d422f63ab61caa
uzimasam <uzimasamuel1@gmail.com>
2024-09-05 11:32:18 +0300

    first commit

---
+++ src/app/[channel]/(main)/page.tsx
@@ -0,0 +1,32 @@
+import { ProductListByCollectionDocument } from "@/gql/graphql";
+import { executeGraphQL } from "@/lib/graphql";
+import { ProductList } from "@/ui/components/ProductList";
+
+export const metadata = {
+	title: "House of Qacym",
+	description:
+		"House of Qacym is a tactical gear and apparel brand that specializes in high-quality, durable products for outdoor enthusiasts.",
+};
+
+export default async function Page({ params }: { readonly params: { readonly channel: string } }) {
+	const data = await executeGraphQL(ProductListByCollectionDocument, {
+		variables: {
+			slug: "featured-products",
+			channel: params.channel,
+		},
+		revalidate: 60,
+	});
+
+	if (!data.collection?.products) {
+		return null;
+	}
+
+	const products = data.collection?.products.edges.map(({ node: product }) => product);
+
+	return (
+		<section className="mx-auto max-w-7xl p-8 pb-16">
+			<h2 className="sr-only">Product list</h2>
+			<ProductList products={products} />
+		</section>
+	);
+}
