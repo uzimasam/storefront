23bf006355cd2cbc658e82df1df187b48e8a4e3c
uzimasam <uzimasamuel1@gmail.com>
2024-09-11 09:41:06 +0300

    Update checkout link in cart page

--- src/app/[channel]/(main)/cart/CheckoutLink.tsx
+++ src/app/[channel]/(main)/cart/CheckoutLink.tsx
@@ -12,7 +12,7 @@ export const CheckoutLink = ({ disabled, checkoutId, className = "" }: Props) =>
 			data-testid="CheckoutLink"
 			aria-disabled={disabled}
 			onClick={(e) => disabled && e.preventDefault()}
-			href="https://houseofqacym.vercel.app/checkout"
+			href={`/?checkout=${checkoutId}`}
 			className={`inline-block max-w-full rounded border border-transparent bg-neutral-900 px-6 py-3 text-center font-medium text-neutral-50 hover:bg-neutral-800 aria-disabled:cursor-not-allowed aria-disabled:bg-neutral-500 sm:px-16 ${className}`}
 		>
 			Checkout Coming Soon
