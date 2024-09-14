495790cc8259037f96c6e5b05ab4fa888ae3c295
uzimasam <uzimasamuel1@gmail.com>
2024-09-14 09:09:58 +0300

    Fix useParams type casting in LinkWithChannel component

--- src/ui/atoms/LinkWithChannel.tsx
+++ src/ui/atoms/LinkWithChannel.tsx
@@ -7,7 +7,8 @@ export const LinkWithChannel = ({
 	href,
 	...props
 }: Omit<ComponentProps<typeof Link>, "href"> & { href: string }) => {
-	const { channel } = useParams<{ channel?: string }>();
+	const { channel } = useParams() as { channel?: string };
+	//const { channel } = useParams<{ channel?: string }>();
 
 	if (!href.startsWith("/")) {
 		return <Link {...props} href={href} />;
