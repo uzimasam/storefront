b2c97600f4c6bd52ac4eb0d732e531dd6eb098b6
uzimasam <uzimasamuel1@gmail.com>
2024-09-30 11:08:47 +0300

    req

--- src/app/api/booking/callback/[code]/route.ts
+++ src/app/api/booking/callback/[code]/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server';
 import { Resend } from 'resend';
 import { AdminEmail, ClientEmail } from '@/app/components/email';
 import prisma from '@/lib/server';
